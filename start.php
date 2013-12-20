4513f9ebc6ca564f6289090633c8f459d2c9ef1b
Juho Jaakkola <juho.jaakkola@mediamaisteri.com>
2013-12-20 18:01:31 +0200

    Added support for more informatic comment notification messages.
    Now the notification messages make use of the translations provided by each content plugin:
    
    Before:
        "Foo commented Bar"
    Now:
        "Foo commented the blog Bar"

--- start.php
+++ start.php
@@ -148,6 +148,7 @@ function notifier_notification_send($hook, $type, $result, $params) {
 	$ia = elgg_set_ignore_access(true);
 
 	$object = $event->getObject();
+	$string = "river:create:{$object->getType()}:{$object->getSubtype()}";
 	$recipient = $notification->getRecipient();
 	$actor = $event->getActor();
 	switch ($object->getType()) {
@@ -162,6 +163,7 @@ function notifier_notification_send($hook, $type, $result, $params) {
 			if ($object instanceof ElggComment) {
 				// Use comment's container as notification target
 				$entity = $object->getContainerEntity();
+				$string = "river:comment:{$entity->getType()}:{$entity->getSubtype()}";
 
 				// TODO How about discussion replies?
 			} else {
@@ -182,8 +184,6 @@ function notifier_notification_send($hook, $type, $result, $params) {
 			// TODO Update time_created?
 		}
 	}
-
-	$string = "river:create:{$object->getType()}:{$object->getSubtype()}";
 
 	// Use summary string if river string is not available
 	if ($string == elgg_echo($string) && !empty($notification->summary)) {
